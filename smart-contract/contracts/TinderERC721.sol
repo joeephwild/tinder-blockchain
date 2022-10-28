3fc1e48ef5b88cde8371931f10bc4939e952d563
joeephwild <josephomotade0@gmail.com>
2022-10-28 19:53:10 +0000

    depoy contract

--- smart-contract/contracts/TinderERC721.sol
+++ smart-contract/contracts/TinderERC721.sol
@@ -1,23 +1,23 @@
-//SPDX-License-Identifier: UNLINCENSED
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
 
-import '@openzepplin/contracts/utils/Counters.sol';
-import '@openzepplin/contracts/token/ERC721/extentions/ERC721URLStorage.sol';
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract TinderERC721 is ERC721URLStorage {
+contract TinderERC721 is ERC721URIStorage {
     
     uint256 TINDER_TOKEN_ID;
 
-    constructor() ERC721("CleverNFT", "CN") {}
+    constructor() ERC721("TinderERC721", "TND") {}
 
-    function mintNft(address _userOne, address _userTwo, string memory, tokenURI) public {
+    function minNFT(address _userOne, address _userTwo, string memory _tokenURI) public {
         _mint(_userOne, TINDER_TOKEN_ID);
-        _setTokenURI(TINDER_TOKEN_ID, tokenURI);
+        _setTokenURI(TINDER_TOKEN_ID, _tokenURI);
         TINDER_TOKEN_ID++;
 
         _mint(_userTwo, TINDER_TOKEN_ID);
-        _setTokenURI(TINDER_TOKEN_ID, tokenURI);
+        _setTokenURI(TINDER_TOKEN_ID, _tokenURI);
         TINDER_TOKEN_ID++;
     }
 }
